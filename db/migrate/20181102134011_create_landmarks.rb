68c2a329453490703729dc14a9a9950011ecbf63
Michael Roose <mjroose@gmail.com>
2018-11-02 13:44:56 +0000

    automatically backed up by learn

--- db/migrate/20181102134011_create_landmarks.rb
+++ db/migrate/20181102134011_create_landmarks.rb
@@ -1,4 +1,9 @@
 class CreateLandmarks < ActiveRecord::Migration
   def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.belongs_to :figure, index: true
+      t.string :year_completed
+    end
   end
 end
