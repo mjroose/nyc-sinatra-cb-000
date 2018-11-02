b72a51d25aa726c10c8e0816c3face5238c95e9e
Michael Roose <mjroose@gmail.com>
2018-11-02 14:38:14 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -9,7 +9,7 @@ class LandmarksController < ApplicationController
 
   get '/landmarks/new' do
     @titles = Title.all
-    @figures = Landmark.all
+    @figures = Figure.all
     erb :'/landmarks/new'
   end
 
