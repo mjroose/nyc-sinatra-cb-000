61bf958f915415bd9d9e11130040831519fbab9b
Michael Roose <mjroose@gmail.com>
2018-11-02 17:03:49 +0000

    automatically backed up by learn

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -4,6 +4,11 @@ class FiguresController < ApplicationController
   set :views, proc { File.join(root, '../views/') }
   register Sinatra::Twitter::Bootstrap::Assets
 
+  get '/' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
   get '/figures/:id' do
     @figure = Figure.find_by(id: params[:id])
     erb :'/figures/show'
