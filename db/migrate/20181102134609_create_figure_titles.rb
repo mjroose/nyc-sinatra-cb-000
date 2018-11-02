f8db70c0d2701d5576ef5e002b5002bd8007d969
Michael Roose <mjroose@gmail.com>
2018-11-02 13:47:12 +0000

    automatically backed up by learn

--- db/migrate/20181102134609_create_figure_titles.rb
+++ db/migrate/20181102134609_create_figure_titles.rb
@@ -1,4 +1,8 @@
 class CreateFigureTitles < ActiveRecord::Migration
   def change
+    create_table :figure_tables do |t|
+      t.belongs_to :title, index: true
+      t.belongs_to :figure, index: true
+    end
   end
 end
