416057c0fa37c39b2a0ea4f08375c315d7b2b357
Michael Roose <mjroose@gmail.com>
2018-11-02 16:16:34 +0000

    automatically backed up by learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,4 +1,4 @@
-require_relative './../models/concerns/LandmarkHelpers.rb'
+require_relative './../models/concerns/LandmarkHelpers'
 
 class LandmarksController < ApplicationController
   set :views, proc { File.join(root, '../views/') }
